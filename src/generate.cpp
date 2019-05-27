b5970bda1689a9e6fc1bc8616c01118c827f11a4
Alexander Elfimow <my.free.mail@gmx.de>
2019-05-27 22:34:54 +0200

    Code to generate functions

--- src/generate.cpp
+++ src/generate.cpp
@@ -7,11 +7,9 @@
 static void gen_WindowsFunc()
 {
     // Function name to be generated
-    const std::string func_name { "execute_cpuid" };
-    comment("void " + func_name + "(size_t RAX_value, size_t RCX_value, size_t *pOut)");
-    comment("RAX_value is in %rcx");
-    comment("RCX_value is in %rdx");
-    comment("pOut is in %r8");
+    const std::string func_name { "rdrand_func" };
+    comment("void " + func_name + "(size_t *pOut)");
+    comment("pOut is in %rcx");
 
     global(func_name);
 
@@ -20,46 +18,16 @@ static void gen_WindowsFunc()
 
     label(func_name);
 
-    r64 &reg_to_save = RBX;
-    PUSH(reg_to_save);  // this register must be saved (and restored later)
-
-    MOV(RAX, RCX);  // RAX_value
-    MOV(RCX, RDX);  // RCX_value
-
-    CPUID();
-
-    size_t offset { 0 };
-    size_t step { 8 };
-    m64 buffer { R8 };
-
-    buffer.disp(offset);
-    MOV(buffer, RAX);
-
-    offset += step;
-    buffer.disp(offset);
-    MOV(buffer, RBX);
-
-    offset += step;
-    buffer.disp(offset);
-    MOV(buffer, RCX);
-
-    offset += step;
-    buffer.disp(offset);
-    MOV(buffer, RDX);
-
-    POP(reg_to_save);   // restore register
-
+    RDRAND(RAX);
     RET();
 }
 
 static void gen_LinuxFunc()
 {
     // Function name to be generated
-    const std::string func_name { "execute_cpuid" };
-    comment("void " + func_name + "(size_t RAX_value, size_t RCX_value, size_t *pOut)");
-    comment("RAX_value is in %rdi");
-    comment("RCX_value is in %rsi");
-    comment("pOut is in %rdx");
+    const std::string func_name { "rdrand_func" };
+    comment("void " + func_name + "(size_t *pOut)");
+    comment("pOut is in %rdi");
 
     global(func_name);
 
@@ -68,36 +36,7 @@ static void gen_LinuxFunc()
 
     label(func_name);
 
-    r64 &reg_to_save = RBX;
-    PUSH(reg_to_save);  // this register must be saved (and restored later)
-
-    MOV(R8, RDX);   // move pOut, because RDX will be overwritten by CPUID
-    MOV(RAX, RDI);  // RAX_value
-    MOV(RCX, RSI);  // RCX_value
-
-    CPUID();
-
-    size_t offset { 0 };
-    size_t step { 8 };
-    m64 buffer { R8 };
-
-    buffer.disp(offset);
-    MOV(buffer, RAX);
-
-    offset += step;
-    buffer.disp(offset);
-    MOV(buffer, RBX);
-
-    offset += step;
-    buffer.disp(offset);
-    MOV(buffer, RCX);
-
-    offset += step;
-    buffer.disp(offset);
-    MOV(buffer, RDX);
-
-    POP(reg_to_save);   // restore register
-
+    RDRAND(RAX);
     RET();
 }
 
