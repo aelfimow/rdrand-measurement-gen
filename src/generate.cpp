50839bd4b52e7106d298be3bf3ff1c79c6f46f8d
Alexander Elfimow <my.free.mail@gmx.de>
2019-05-31 22:54:32 +0200

    Generated further functions

--- src/generate.cpp
+++ src/generate.cpp
@@ -33,7 +33,7 @@ static void gen_function(std::string const &func_name, r64 &param, size_t max_co
     for (size_t count = 0; count < max_count; ++count)
     {
         std::stringstream ss;
-        ss << "rn" << count;
+        ss << func_name + "_rn" << count;
         std::string rnx { ss.str() };
 
         label(rnx);
@@ -79,11 +79,29 @@ try
     if (forWindows)
     {
         gen_function("rdrand_func1", RCX, 1);
+        gen_function("rdrand_func4", RCX, 4);
+        gen_function("rdrand_func8", RCX, 8);
+        gen_function("rdrand_func16", RCX, 16);
+        gen_function("rdrand_func32", RCX, 32);
+        gen_function("rdrand_func64", RCX, 64);
+        gen_function("rdrand_func128", RCX, 128);
+        gen_function("rdrand_func256", RCX, 256);
+        gen_function("rdrand_func512", RCX, 512);
+        gen_function("rdrand_func1024", RCX, 1024);
     }
 
     if (forLinux)
     {
         gen_function("rdrand_func1", RDI, 1);
+        gen_function("rdrand_func4", RDI, 4);
+        gen_function("rdrand_func8", RDI, 8);
+        gen_function("rdrand_func16", RDI, 16);
+        gen_function("rdrand_func32", RDI, 32);
+        gen_function("rdrand_func64", RDI, 64);
+        gen_function("rdrand_func128", RDI, 128);
+        gen_function("rdrand_func256", RDI, 256);
+        gen_function("rdrand_func512", RDI, 512);
+        gen_function("rdrand_func1024", RDI, 1024);
     }
 
     return EXIT_SUCCESS;
